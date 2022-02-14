e30761f097575333d8362c39d2291d1af297a351
johannes545 <Johannes545@gmail.com>
2022-02-14 17:28:53 +0100

    area of circle method

---
+++ console app operators/console app operators/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace console_app_operators
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int num1 = 17;
+            int num2 = 4;
+
+            int answer = num1 %= num2;
+
+            Console.WriteLine(answer);
+
+
+            double pi = 3.14;
+            Console.WriteLine("what is the radius of your circle?");
+            double radius = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"the area of a circle with the radius of 20 is { (AreaOfCircle(pi, radius))}");
+            
+           
+        }
+
+
+        public static double AreaOfCircle(double pi, double radius)
+        {
+
+            return (pi * radius * radius);
+         
+        }
+
+
+    }
+}
